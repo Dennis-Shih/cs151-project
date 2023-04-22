82ecb5a76d5785b8fb4445f4e3bc8190fef60210
Aung Paing Soe <aungpaingsoe03@gmail.com>
2023-04-21 21:14:12 -0700

    Started the backend program for MancalaBoard and Player

---
+++ Player.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class Player {
+    public static final int PIT_SIZE = 6;
+    private ArrayList<Integer> pits;
+    private int finalScore;
+
+    public Player() {
+        pits = new ArrayList<>();
+        finalScore = 0;
+    }
+    public int getFinalScore(){
+        return finalScore;
+    }
+
+    public void addScore(int scoreToAdd){
+        finalScore += scoreToAdd;
+    }
+
+    public void addStone(int stoneToAdd, int startingPitNum){
+        int temp = 1;
+        for(int i = startingPitNum; i < pits.size(); i++){
+            pits.set(i, temp);
+            temp++;
+        }
+        if (temp > 0){
+            addScore(1);
+            temp--;
+        }
+    }
+
+    // Done at the beginning of the game
+    public void setStone(int stones){
+        for (int i = 0; i < PIT_SIZE; i++){
+            pits.add(stones);
+        }
+    }
+}
